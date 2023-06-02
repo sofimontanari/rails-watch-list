a092bfd0ce6d0316b98484ac86c2f79210cf3fb7
Sofia Montanari <sofiamontanari.a@gmail.com>
2023-06-02 18:02:47 -0300

    estilos

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'bookmarks/new'
   get 'bookmarks/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: 'lists#index'
   resources :lists, only: [:index, :show, :new, :create] do
     resources :bookmarks, only:[:new, :create ]
   end
